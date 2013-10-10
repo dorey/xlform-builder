e62ee3a77e7951e808d18b0158248ee2056c54cb
Alex Dorey <dorey415@gmail.com>
2013-10-09 17:01:07 -0700

    renamed jquery editinplace plugin
    the filename was improperly referencing the wrong edit-in-place plug in.
    This is the good one:
    http://jquery-in-place-editor.googlecode.com/svn/trunk/demo/index.html

--- source/javascripts/libs.js
+++ source/javascripts/libs.js
@@ -1,5 +1,5 @@
 //= require ./lib/csv.coffee
 //= require ./lib/jquery.js
-//= require ./lib/jquery.jeditable.js
+//= require ./lib/jquery.editinplace.js
 //= require ./lib/underscore.js
 //= require ./lib/backbone.js
